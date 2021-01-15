adc9c822e8f2092e5233a7e4d75dcff9b8bdc53c
jeanhern81 <jeanhern81@gmail.com>
2021-01-15 14:41:32 -0800

    added content to auto quote

--- Public/js/script.js
+++ Public/js/script.js
@@ -8,3 +8,13 @@ function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
 
+
+
+$(document).ready(function() {
+    $('#bt1').click(function() {
+        $('#fr1').attr('action',
+        'mailto:jeanhern81@gmail.com?subject=' +
+        $('#tb1').val() + '&body=' + $('#tb2').val());
+        $('#fr1').submit();
+    });
+});
